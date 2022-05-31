c58860fdb4979c9f1ca6ff2c703998c961faaafd
Cristian Yair Restrepo Escobar <cyrestre@bancolombia.com.co>
2022-05-31 14:54:52 -0500

    "se agregan los demas cambios en donde se eliminan las carpetas "

--- study_projects/maps/src/main/java/Persona.java
+++ study_projects/maps/src/main/java/Persona.java
@@ -1,33 +1,3 @@
 public class Persona {
-    private Integer id;
-    private String nombre;
 
-    public Persona(Integer id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public String toString() {
-        return "Persona{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                '}';
-    }
 }
